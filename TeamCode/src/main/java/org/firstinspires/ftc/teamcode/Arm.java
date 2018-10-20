ab41a1856f61e3c2c3423750fefaa6f65ce0ef8f
Alec <awsanchez1@gmail.com>
2018-10-20 13:37:13 -0500

    arm starting to work
    arm starting to work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -8,6 +9,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 import java.util.LinkedList;
 
+@TeleOp(name="armTest", group = "")
 public class Arm extends LinearOpMode {
 
 
@@ -21,20 +23,23 @@ public class Arm extends LinearOpMode {
     Double lastTimeY = 0.0;
     Double lastTimeX = 0.0;
 
-    final static double SERVO_B_POS_0 = 0;
-    final static double SERVO_C_POS_0 = 0;
+    final static int MOTOR_A_POS_0 = 420;
+    final static double SERVO_B_POS_0 = 0.633;
+    final static double SERVO_C_POS_0 = 0.816;
     final static double SERVO_D_POS_0 = 0;
 
-    final static double SERVO_B_POS_90 = 0;
-    final static double SERVO_C_POS_90= 0;
+    final static int MOTOR_A_POS_90 = 0;
+    final static double SERVO_B_POS_90 = 0; //45 deg
+    final static double SERVO_C_POS_90= 0.265; //-90 deg
     final static double SERVO_D_POS_90 = 0;
 
-    final static double SERVO_B_CONVERSION_FACTOR = (SERVO_B_POS_90-SERVO_B_POS_0)/90;
-    final static double SERVO_C_CONVERSION_FACTOR = (SERVO_C_POS_90-SERVO_C_POS_0)/90;
+    final static double MOTOR_A_CONVERSION_FACTOR = (MOTOR_A_POS_90 - MOTOR_A_POS_90)/90;
+    final static double SERVO_B_CONVERSION_FACTOR = (SERVO_B_POS_90-SERVO_B_POS_0)/45;
+    final static double SERVO_C_CONVERSION_FACTOR = (SERVO_C_POS_90-SERVO_C_POS_0)/-90;
     final static double SERVO_D_CONVERSION_FACTOR = (SERVO_D_POS_90-SERVO_D_POS_0)/90;
 
     @Override
-    public void runOpMode() {
+    public void runOpMode() throws InterruptedException {
 
         servoB = hardwareMap.get(Servo.class, "B");
         servoC = hardwareMap.get(Servo.class, "C");
@@ -52,12 +57,17 @@ public class Arm extends LinearOpMode {
         telemetry.addLine("Ready to go.");
         telemetry.update();
 
+        motorA.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorA.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorA.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
         waitForStart();
 
 
         while(opModeIsActive()){
 
             double phi = servoD.getPosition();
+
 
             if(gamepad1.left_stick_y != 0 && (lastTimeX - runner.milliseconds()) > 300){
                 x = x -gamepad1.left_stick_y * 0.01;
@@ -81,24 +91,32 @@ public class Arm extends LinearOpMode {
 
             }
 
-            LinkedList<Double> values = invKinematics(x,y,phi);
+            //LinkedList<Double> values = invKinematics(x,y,phi);
 
-            a = values.get(0);
-            b = values.get(1);
-            c = values.get(2);
-
-
-            servoB.setPosition(a);
-            servoC.setPosition(b);
-            servoD.setPosition(c);
+            //a = values.get(0);
+            //b = values.get(1);
+            //c = values.get(2);
 
 
+            servoB.setPosition(gamepad1.left_stick_x);
+            servoC.setPosition(gamepad1.left_stick_y);
+            servoD.setPosition(gamepad1.right_stick_x);
+            motorA.setTargetPosition(200);
+            //motorA.setTargetPosition((int) (MOTOR_A_POS_0 + 45*MOTOR_A_CONVERSION_FACTOR + 45*gamepad1.right_stick_y*MOTOR_A_CONVERSION_FACTOR));
+
+            //motorA.se
             telemetry.addData("SERVO B", servoB.getPosition());
             telemetry.addData("SERVO C", servoC.getPosition());
             telemetry.addData("SERVO D", servoD.getPosition());
-            telemetry.addData("X", x);
-            telemetry.addData("Y", y);
+            telemetry.addData("MOTOR A", motorA.getCurrentPosition());
+            telemetry.addData("MOTOR A TARGET POSITION", motorA.getTargetPosition());
+            telemetry.addData("PID COEF", motorA.getPIDFCoefficients(DcMotor.RunMode.RUN_TO_POSITION));
+            telemetry.addData("runmode",motorA.getMode());
+            telemetry.addData("setPower",motorA.getPower());
 
+
+
+            telemetry.update();
         }
 
 
@@ -107,7 +125,7 @@ public class Arm extends LinearOpMode {
 
 
 
-    }
+    }/*
     public LinkedList<Double> invKinematics(double x, double y, double phi){
             LinkedList<Double> angles = new LinkedList<Double>(); //initializes list that outputs will be added to
             //joint angles will folow q1, q2, ... pattern
@@ -137,5 +155,5 @@ public class Arm extends LinearOpMode {
             angles.add(q3);
 
             return angles; //angles outputs in radians
-    }
+    }*/
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousPrototype1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousPrototype1.java
@@ -34,4 +34,4 @@ public class AutonomousPrototype1 extends LinearOpMode {
             }
         }
     }
-}
+
